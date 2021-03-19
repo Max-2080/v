31c8959eb85747912b49f10f370244f97c253dd6
Max-2080 <75639270+Max-2080@users.noreply.github.com>
2021-03-19 20:47:47 +0300

    Add files via upload

---
+++ class.py
@@ -0,0 +1,37 @@
+
+
+
+a=int(input("Ведите количество на складе"))
+d=int(input("Введите цену"))
+s=int(input("Сколько купить"))
+h=str(input("Введите название товара"))
+o=d*s
+class towar_w_magazine ():
+    def __init__(self, imya , cena, kolichectwo,  ):
+        self.imya = imya
+        self.cena = cena
+        self.kolichectwo = kolichectwo
+
+
+    def cklad (self, na_cklade):
+        print("На складе=", a )
+
+
+    def cena (self, cena):
+        print("Цена=",d)
+
+
+    def kypit (self, kol_wo):
+        print("Количество=" , s )
+
+
+    def kypit_towar (self, kypit):
+        g=a-s
+        if g < 0:
+            print("Невозможно")
+        if g==str:
+            print("Невозможно")
+        if g > 0:
+            print("Приобретено", s, h , d , o)
+
+
